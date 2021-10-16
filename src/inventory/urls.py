491b4e1adcc408b0f5b5b8ba1befa42ed81683c3
jmateo <jmateo.cys@gmail.com>
2021-10-16 14:12:16 -0600

    Editar cantidad del lote

--- src/inventory/urls.py
+++ src/inventory/urls.py
@@ -20,7 +20,7 @@ from inventory.index.views import index, addgraph
 from inventory.products.views import *
 from inventory.users.views import adduser, newuser, listuser, deleteuser, edituser
 from inventory.clients.views import modal_register_client
-from inventory.products.views import modify_order_product, create_product_type, list_categories, listproduct, list_products_supplier, send_order_email,  manual_purchase
+from inventory.products.views import modify_order_product, create_product_type, list_categories, listproduct, listlot, list_products_supplier, send_order_email,  manual_purchase
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
@@ -36,6 +36,9 @@ urlpatterns = [
 
     path('manager/listproduct/', listproduct, name='listproduct'),
     path('manager/manual_purchase/<int:id>/', manual_purchase, name='manual_purchase'),
+
+    path('manager/listlot/<int:id>/', listlot, name='list_lot'),
+
     
     # Paths para las funciones de manager 
     path('manager/edit_product/<int:id>/', modify_order_product, name='edit_product'),
